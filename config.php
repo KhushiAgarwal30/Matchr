f53575bbfb5014a49b7e9561946c8d20e4f1f4e0
KhushiAgarwal <64637893+KhushiAgarwal30@users.noreply.github.com>
2021-07-07 00:02:06 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $user = "root";
 $pass = "";
 $database = "login_register_pure_coding";
 
-$conn = mysqli_connect($server, $user, $pass, $database);
+$conn = mysqli_connect($server, "id17194878_project","x2rmqy6?<bhuq~W", "id17194878_matchr");
 
 if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
